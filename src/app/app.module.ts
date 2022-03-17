f07f9feb07a9b4c7e5010ce9dc5677cbcb6f8165
Natalia Norboeva <divonata@gmail.com>
2022-03-17 17:01:23 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateItemComponent } from './components/create-item/create-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateItemComponent
   ],
   imports: [
     BrowserModule
